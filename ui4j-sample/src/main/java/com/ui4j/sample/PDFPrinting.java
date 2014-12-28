89630c497a9a6fb15b764b1e520549b734f85f60
ui4j <admin@atolyemayis.com>
2014-12-28 16:25:10 +0200

    Fix #13 Added sample for PDF printing

---
+++ ui4j-sample/src/main/java/com/ui4j/sample/PDFPrinting.java
@@ -0,0 +1,64 @@
+package com.ui4j.sample;
+
+import java.util.Iterator;
+
+import javafx.print.PageLayout;
+import javafx.print.PageOrientation;
+import javafx.print.Paper;
+import javafx.print.Printer;
+import javafx.print.Printer.MarginType;
+import javafx.print.PrinterJob;
+import javafx.scene.web.WebEngine;
+
+import com.ui4j.api.browser.BrowserFactory;
+import com.ui4j.api.browser.Page;
+
+public class PDFPrinting {
+
+    public static void main(String[] args) {
+
+        Printer pdfPrinter = null;
+
+        // Windows: Instal PDF24 from http://en.pdf24.org
+        // Linux: Install cups-pdf http://www.cups-pdf.de
+
+        // Optionally PDF24 could be configured for silent mode from
+        // "C:\Program Files (x86)\PDF24\pdf24-SettingsUITool.exe" from the Menu "PDF Printer"
+        // Set "Automatically save..." checkbox to true
+
+        Iterator<Printer> iter = Printer.getAllPrinters().iterator();
+        while (iter.hasNext()) {
+            Printer printer = iter.next();
+            if (printer.getName().endsWith("PDF")) {
+                pdfPrinter = printer;
+            }
+        }
+
+        String url = PDFPrinting.class.getResource("/sample-page.html").toExternalForm();
+
+        Page page = BrowserFactory
+            .getWebKit()
+            .navigate(url);
+
+        WebEngine engine = (WebEngine) page.getEngine();
+
+        PrinterJob job = null;
+        try {
+            page.show();
+            // clear margins
+            PageLayout layout = pdfPrinter.createPageLayout(Paper.A4, PageOrientation.PORTRAIT, MarginType.HARDWARE_MINIMUM);
+
+            job = PrinterJob.createPrinterJob(pdfPrinter);
+            job.getJobSettings().setPageLayout(layout);
+            job.getJobSettings().setJobName("Sample Printing Job");
+
+            engine.print(job);
+            job.endJob();
+        } finally {
+            if (job != null) {
+                job.endJob();
+            }
+            page.close();
+        }
+    }
+}
