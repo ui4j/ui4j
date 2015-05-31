fd0ab2844b58f06aaea7e926d5bc317840243c09
ui4j <admin@atolyemayis.com>
2015-05-31 21:03:05 +0300

    #33 test Element.setInnerHTML

---
+++ ui4j-webkit/src/test/java/com/ui4j/test/SetInnerHTMLTest.java
@@ -0,0 +1,24 @@
+package com.ui4j.test;
+
+import org.junit.Test;
+
+import com.ui4j.api.browser.BrowserEngine;
+import com.ui4j.api.browser.BrowserFactory;
+import com.ui4j.api.browser.Page;
+import com.ui4j.api.dom.Element;
+
+import static org.junit.Assert.assertEquals;
+
+public class SetInnerHTMLTest {
+
+	@Test
+	public void test() throws InterruptedException {
+		BrowserEngine webkit = BrowserFactory.getWebKit();
+		try (Page page = webkit.navigate(ChildTest.class.getResource("/SetInnerTest.html").toExternalForm())) {
+			Element withInner = page.getDocument().query("#with-inner");
+			withInner.setInnerHTML("<span id=\"blub\"></span>");
+			String innerHTML = withInner.getInnerHTML();
+			assertEquals(innerHTML, "<span id=\"blub\"></span>");
+		}
+	}
+}
