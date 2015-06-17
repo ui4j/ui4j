ee581351dabc96097e78c213b3d961735daea1e7
ui4j <admin@atolyemayis.com>
2015-06-17 11:51:35 +0300

    #55 use cookie manager for the client side cookie test

--- ui4j-webkit/src/test/java/com/ui4j/test/ClientSideCookieTest.java
+++ ui4j-webkit/src/test/java/com/ui4j/test/ClientSideCookieTest.java
@@ -1,6 +1,7 @@
 package com.ui4j.test;
 
 import java.io.IOException;
+import java.net.CookieHandler;
 import java.net.HttpCookie;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +10,16 @@ import org.junit.Test;
 
 import com.ui4j.api.browser.BrowserFactory;
 import com.ui4j.api.browser.Page;
+
 import static org.junit.Assert.assertEquals;
+
+import com.sun.webkit.network.CookieManager;
 
 public class ClientSideCookieTest extends AbstractFileServerTest {
 
     @Test
     public void test() throws IOException, InterruptedException {
+    	CookieHandler.setDefault(new CookieManager());
         Page page = BrowserFactory.getWebKit().navigate("http://localhost:58844/ClientSideCookieTest.html");
         System.out.println(page.executeScript("typeof document"));
         System.out.println(page.executeScript("typeof document.cookie"));
