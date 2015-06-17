86c83b3236e057131c184418024a3deb1e913441
ui4j <admin@atolyemayis.com>
2015-06-17 12:01:42 +0300

    #55 Fix NullPointerException and always use Locale.ENGLISH to parse numeric values

--- ui4j-webkit/src/main/java/com/ui4j/webkit/spi/WebKitJavaScriptEngine.java
+++ ui4j-webkit/src/main/java/com/ui4j/webkit/spi/WebKitJavaScriptEngine.java
@@ -1,5 +1,9 @@
 package com.ui4j.webkit.spi;
 
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+import java.util.Locale;
+
 import javafx.scene.web.WebEngine;
 
 import com.ui4j.spi.JavaScriptEngine;
@@ -20,6 +24,22 @@ public class WebKitJavaScriptEngine implements JavaScriptEngine {
     @Override
     public Object executeScript(String script) {
         Object result = engine.executeScript(script);
+
+        String resultStr = String.valueOf(result);
+
+        try {
+            NumberFormat formatter = NumberFormat.getInstance(Locale.ENGLISH);
+            ParsePosition pos = new ParsePosition(0);
+            Number number = formatter.parse(resultStr, pos);
+            if (number != null) {
+                if (resultStr.length() == pos.getIndex()) {
+                    return number;
+                }
+            }
+        } catch (Throwable t) {
+            // ignore issue #55
+        }
+
         return result;
     }
 }
--- ui4j-webkit/src/test/java/com/ui4j/test/JavaScriptEngineTest.java
+++ ui4j-webkit/src/test/java/com/ui4j/test/JavaScriptEngineTest.java
@@ -14,7 +14,8 @@ public class JavaScriptEngineTest {
 
     private static Page page;
 
-    @BeforeClass public static void beforeTest() {
+    @BeforeClass
+    public static void beforeTest() {
         String url = ElementTest.class.getResource("/TestPage.html").toExternalForm();
         BrowserEngine browser = BrowserFactory.getBrowser(BrowserType.WebKit);
         PageConfiguration config = new PageConfiguration();
@@ -22,27 +23,44 @@ public class JavaScriptEngineTest {
         page = browser.navigate(url, config);
     }
 
-    @Test public void testBoolean() {
+    @Test
+    public void testBoolean() {
         Object executeScript = page.executeScript("1 === 1");
         Assert.assertEquals(Boolean.class, executeScript.getClass());
     }
 
-    @Test public void testString() {
+    @Test
+    public void testString() {
         Object result = page.executeScript("'test string'");
         Assert.assertEquals(String.class, result.getClass());
     }
 
-    @Test public void testNull() {
+    @Test
+    public void testLong() {
+        Object result = page.executeScript("'20'");
+        Assert.assertEquals(Long.class, result.getClass());
+    }
+
+    @Test
+    public void testDouble() {
+        Object result = page.executeScript("'20.20'");
+        Assert.assertEquals(Double.class, result.getClass());
+    }
+
+    @Test
+    public void testNull() {
         Object result = page.executeScript("null");
         Assert.assertNull(result);
     }
 
-    @Test public void testUndefined() {
+    @Test
+    public void testUndefined() {
         Object result = page.executeScript("undefined");
         Assert.assertEquals("undefined", result);
     }
 
-    @Test public void testUserAgent() {
+    @Test
+    public void testUserAgent() {
         Assert.assertEquals("foo bar", page.executeScript("navigator.userAgent"));
     }
 }
